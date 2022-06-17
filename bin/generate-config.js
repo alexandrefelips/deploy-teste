b2cb332259dc77e93a5754d72d10cc13bc1bf15e
Alexandre <alexandrefsans@gmail.com>
2022-06-17 12:42:44 -0300

    teste de deploy

--- bin/generate-config.js
+++ bin/generate-config.js
@@ -8,13 +8,13 @@ const portalId = process.env.HUBSPOT_PORTAL_ID;
 const apiKey = process.env.HUBSPOT_API_KEY;
 
 const portalConfig = {
-  name: "deploy-test",
+  name: "PROD",
   portalId,
   apiKey,
 };
 
 const config = {
-  defaultPortal: "deploy-test",
+  defaultPortal: "PROD",
   portals: [portalConfig],
 };
 
